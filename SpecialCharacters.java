d9e16f2d2b4497b4040b7be618f3f830a72454b8
abdul rahman <abdul19rehmankhan@gamil.com>
2025-09-10 16:45:31 +0530

    Day 1

---
+++ SpecialCharacters.java
@@ -0,0 +1,21 @@
+
+class SpecialCharacters {
+    public static void main(String[] args) {
+        String s = "&ABCef@$gmj@#$";
+        int cnt = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (
+                    !(ch >= 'A' && ch <= 'Z') &&
+
+                            !(ch >= 'a' && ch <= 'z') &&
+
+                            !(ch >= '0' && ch <= '9')
+            ) {
+                cnt++;
+            }
+        }
+        System.out.println("Number of special characters is " + cnt);
+    }
+}
